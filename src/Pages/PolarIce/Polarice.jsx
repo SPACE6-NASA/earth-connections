5301fbfc43ba6939174f114a8fe013e5369c159c
Jhay Savi <jansavinon@outlook.com>
2024-10-06 15:45:11 -0300

    add embed polar ice

--- src/Pages/PolarIce/Polarice.jsx
+++ src/Pages/PolarIce/Polarice.jsx
@@ -5,12 +5,22 @@ function PolarIce() {
   const polarIceInfo = {
     title: 'Polar Ice Melting',
     description: 'See how melting polar ice impacts sea levels and global climate patterns.',
-    imageUrl: '/path/to/polar-ice-image.jpg'
   };
 
   return (
     <div>
-      <Section title={polarIceInfo.title} description={polarIceInfo.description} imageUrl={polarIceInfo.imageUrl} />
+      <Section
+        title={polarIceInfo.title}
+        description={polarIceInfo.description}
+      />
+      <div style={{ width: '100%', height: '600px' }}>
+        <iframe
+          width="80%"
+          height="400"
+          src="https://earthengine.google.com/iframes/timelapse_player_embed.html#v=-75.69097,83.16553,1.914,latLng&t=1.78&ps=50&bt=19840101&et=20221231"
+          allowFullScreen
+        ></iframe>
+      </div>
       <Simulator />
     </div>
   );
